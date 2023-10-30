94c6789dbdcd5824c51fc38c94b1de0516628bd4
Alex Barbero <barrufat@hotmail.com>
2023-10-30 20:20:26 +0100

    Add Header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
+import Header from "../Header/Header";
 import AppStyled from "./AppStyled";
 
 const App = () => {
-  return <AppStyled />;
+  return (
+    <AppStyled>
+      <Header />
+    </AppStyled>
+  );
 };
 
 export default App;
