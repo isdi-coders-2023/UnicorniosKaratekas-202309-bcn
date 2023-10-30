98dd8fa0a27af252b9dbe8bb1413e58a4a57f05d
Berta González <bertagonzalezmartin@gmail.com>
2023-10-30 19:54:57 +0100

    Cherrypick of main

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import "@fontsource/libre-baskerville";
+import GlobalStyle from "./styles/GlobalStyle";
+import mainTheme from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
