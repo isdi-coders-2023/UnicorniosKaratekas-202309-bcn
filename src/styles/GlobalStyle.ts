1ba97d9f3a21d5987383ac3cc45ed5d14ea57e83
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-30 18:26:18 +0100

    Add spaces

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,13 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
 *, ::before, ::after {
-box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 body {
-margin: 0;
+  margin: 0;
 }
 
 ul,
@@ -18,18 +17,18 @@ li {
   } 
 
 a {
-text-decoration: none;
-color: inherit;
+  text-decoration: none;
+  color: inherit;
 }
 
 button,
 input {
-    font-family: inherit;
+  font-family: inherit;
   }
 
 h1,
 h2 {
-    margin: 0;
+  margin: 0;
   }
 
 button {
