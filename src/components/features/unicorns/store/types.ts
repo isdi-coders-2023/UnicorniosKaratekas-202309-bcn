39be8d67c6e4169b5a63d00f2b060753eb49269b
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-31 12:24:16 +0100

    Delete unused types

--- src/components/features/unicorns/store/types.ts
+++ src/components/features/unicorns/store/types.ts
@@ -2,10 +2,6 @@ import { Unicorn } from "../../../../types";
 
 interface UnicornContextStructure {
   unicorns: Unicorn[];
-  loadUnicorns: (unicorns: Unicorn[]) => void;
-  addUnicorn: (unicorn: Unicorn[]) => void;
-  deleteUnicorn: (unicorn: Unicorn[]) => void;
-  modifyUnicorn: (unicorn: Unicorn[]) => void;
 }
 
 export default UnicornContextStructure;
