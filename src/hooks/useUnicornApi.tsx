d1c23f83e076f2cca7c4f4494ef7c06bcfa2a743
Alex Barbero <barrufat@hotmail.com>
2023-11-01 13:08:58 +0100

    Add mock service worker configurations

--- src/hooks/useUnicornApi.tsx
+++ src/hooks/useUnicornApi.tsx
@@ -1,15 +1,14 @@
-import { useCallback } from "react";
-import UnicornContextStructure from "../components/features/unicorns/store/types";
+import { Unicorn } from "../types";
 
 const useUnicornsApi = () => {
   const apiUrl = import.meta.env.VITE_SWAPI_URL;
 
-  const getUnicorns = useCallback(async () => {
+  const getUnicorns = async () => {
     const response = await fetch(`${apiUrl}?_limit=10`);
-    const unicorns = (await response.json()) as UnicornContextStructure;
+    const unicorns = (await response.json()) as Unicorn[];
 
     return unicorns;
-  }, [apiUrl]);
+  };
 
   return {
     getUnicorns,
