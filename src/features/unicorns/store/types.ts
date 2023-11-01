5567dbe954b655e4438aa576ac9f6d635068889a
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-01 16:47:40 +0100

    Move features folder to src

---
+++ src/features/unicorns/store/types.ts
@@ -0,0 +1,8 @@
+import { Unicorn } from "../../../types";
+
+interface UnicornContextStructure {
+  unicorns: Unicorn[];
+  loadUnicorns: (unicorn: Unicorn[]) => void;
+}
+
+export default UnicornContextStructure;
