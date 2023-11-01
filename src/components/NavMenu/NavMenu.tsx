a281aafb2da349da61103747d7ff2ed5188d7787
Berta González <bertagonzalezmartin@gmail.com>
2023-11-01 11:21:04 +0100

    Fix nav menu component and styles

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,23 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faPlus } from "@fortawesome/free-solid-svg-icons";
 import NavMenuStyled from "./NavMenuStyled";
+import { NavLink, useLocation } from "react-router-dom";
 
 const NavMenu = (): React.ReactElement => {
+  const { pathname } = useLocation();
   return (
     <NavMenuStyled>
-      <div className="nav-container">
-        <a href="/unicorns-list">
-          <FontAwesomeIcon className="button" icon={faHouse} />
-        </a>
-        <a href="/unicorns-list">
-          <FontAwesomeIcon className="button" icon={faPlus} />
-        </a>
+      <div className="button-container">
+        <NavLink
+          className={`icon ${pathname === "/unicorns" ? "icon__line" : ""}`}
+          to={"/unicorns"}
+        >
+          <FontAwesomeIcon className="icon button" icon={faHouse} />
+        </NavLink>
+        <NavLink
+          className={`icon ${pathname === "/" ? "icon__line" : ""}`}
+          to={"/"}
+        >
+          <FontAwesomeIcon className="icon button" icon={faPlus} />
+        </NavLink>
       </div>
       <img
-        className="nav-background"
+        className="background-image"
         src="/images/nav-background.svg"
-        alt="navigation menu background"
+        alt="background"
+        width="500"
+        height="95"
       />
     </NavMenuStyled>
   );
