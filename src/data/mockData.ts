40b43377e20fe8efda4d1d57400ea0239c5e25dc
Berta González <bertagonzalezmartin@gmail.com>
2023-11-02 18:59:44 +0100

    Add a unicorn mock for testing

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,6 +1,6 @@
-import { Unicorn } from "../types";
+import { Unicorn } from "../features/unicorns/store/types";
 
-const unicornMock: Unicorn[] = [
+export const unicornsMock: Unicorn[] = [
   {
     id: 42,
     name: "Mr. Shy",
@@ -43,4 +43,16 @@ const unicornMock: Unicorn[] = [
   },
 ];
 
-export default unicornMock;
+export const unicornMock: Unicorn = {
+  id: 42,
+  name: "Mr. Shy",
+  alias: "Pinkamena Diane Pie",
+  url: "https://mlp.fandom.com/wiki/Mr._and_Mrs._Shy",
+  sex: "Male",
+  residence: "Cloudsdale",
+  occupation: "Weather factory worker (formerly)",
+  kind: ["Pegasus"],
+  image: [
+    "https://vignette.wikia.nocookie.net/mlp/images/2/2d/Mr._Shy_ID_S6E11.png/revision/latest?cb=20160605103050",
+  ],
+};
