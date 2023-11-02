687cfe6ce52fa3bba00ac7ed09a24ea069b36889
Berta González <bertagonzalezmartin@gmail.com>
2023-11-02 19:39:48 +0100

    Refact router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AppStyled from "./AppStyled";
 import Header from "../Header/Header";
 import UnicornListPage from "../../pages/UnicornsList/UnicornsList";
 import NavMenu from "../NavMenu/NavMenu";
-import UnicornContext from "../../features/unicorns/store/UnicornContext";
-import unicornMock from "../../data/mockData";
 
 const App = (): React.ReactElement => {
-  const { loadUnicorns } = useContext(UnicornContext);
-
-  useEffect(() => {
-    loadUnicorns(unicornMock);
-  }, [loadUnicorns]);
-
   return (
     <div className="main-container">
       <Header />
       <AppStyled>
         <Routes>
-          <Route path="/" element={<UnicornListPage />} />
+          <Route path="/" element={<Navigate to="/unicorns" />} />
+          <Route path="/unicorns" element={<UnicornListPage />} />
         </Routes>
       </AppStyled>
       <NavMenu />
