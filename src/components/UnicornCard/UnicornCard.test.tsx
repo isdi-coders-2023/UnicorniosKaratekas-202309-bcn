c82eba1859ba5ec1dc9916dabd6f055cb46020f9
Berta González <bertagonzalezmartin@gmail.com>
2023-11-02 19:16:02 +0100

    Refact UnicornCard test

--- src/components/UnicornCard/UnicornCard.test.tsx
+++ src/components/UnicornCard/UnicornCard.test.tsx
@@ -4,17 +4,28 @@ import { ThemeProvider } from "styled-components";
 import UnicornCard from "./UnicornCard";
 import mainTheme from "../../styles/mainTheme";
 import UnicornProviderWrapper from "../../features/unicorns/store/UnicornProviderWrapper";
-import { unicornMock } from "../../data/mockData";
 
 describe("Given a UnicornCard component", () => {
   describe("When it receives a unicorn data", () => {
     test("Then it should show the unicorn's name into a heading", () => {
-      const expectedUnicornName = unicornMock;
+      const expectedUnicornName = {
+        id: 42,
+        name: "Mr. Shy",
+        alias: "Pinkamena Diane Pie",
+        url: "https://mlp.fandom.com/wiki/Mr._and_Mrs._Shy",
+        sex: "Male",
+        residence: "Cloudsdale",
+        occupation: "Weather factory worker (formerly)",
+        kind: ["Pegasus"],
+        image: [
+          "https://vignette.wikia.nocookie.net/mlp/images/2/2d/Mr._Shy_ID_S6E11.png/revision/latest?cb=20160605103050",
+        ],
+      };
 
       render(
         <UnicornProviderWrapper>
           <ThemeProvider theme={mainTheme}>
-            <UnicornCard unicorn={unicornMock} />
+            <UnicornCard unicorn={expectedUnicornName} />
           </ThemeProvider>
         </UnicornProviderWrapper>,
       );
