ba3a834fed4a5b2b8d2f8eb1e9af12304f66e9f0
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-03 20:30:00 +0100

    Add load unicorns function

--- src/hooks/useUnicornApi.test.tsx
+++ src/hooks/useUnicornApi.test.tsx
@@ -1,19 +1,70 @@
-import unicorns from "../mocks/mockData";
 import { renderHook } from "@testing-library/react";
 import { Unicorn } from "../features/unicorns/store/types";
 
 import useUnicornsApi from "./useUnicornApi";
+import UiContextWrapper from "../features/ui/store/UiContextWrapper";
 
 describe("Given a get custom hook", () => {
   describe("When it fetches unicorns from the API", () => {
     test("Then it should return some unicorns", async () => {
-      const expectedUnicorns: Unicorn[] = unicorns;
+      const expectedUnicorns: Unicorn[] = [
+        {
+          id: 42,
+          name: "Mr. Shy",
+          alias: "Pinkamena Diane Pie",
+          url: "https://mlp.fandom.com/wiki/Mr._and_Mrs._Shy",
+          sex: "Male",
+          residence: "Cloudsdale",
+          occupation: "Weather factory worker (formerly)",
+          kind: ["Pegasus"],
+          image: [
+            "https://vignette.wikia.nocookie.net/mlp/images/2/2d/Mr._Shy_ID_S6E11.png/revision/latest?cb=20160605103050",
+          ],
+        },
+        {
+          id: 43,
+          name: "Mrs. Shy",
+          alias: "Pinkamena Diane Pie",
+          url: "https://mlp.fandom.com/wiki/Mr._and_Mrs._Shy",
+          sex: "Female",
+          residence: "Cloudsdale",
+          occupation: "Weather factory worker (formerly)",
+          kind: ["Pegasus"],
+          image: [
+            "https://vignette.wikia.nocookie.net/mlp/images/1/1a/Mrs._Shy_ID_S6E11.png/revision/latest?cb=20160605103103",
+          ],
+        },
+        {
+          id: 44,
+          name: "Zephyr Breeze",
+          alias: "Pinkamena Diane Pie",
+          url: "https://mlp.fandom.com/wiki/Zephyr_Breeze",
+          sex: "Male",
+          residence: "Cloudsdale\nPonyville (briefly in S6E11)",
+          occupation:
+            "Mane therapist\nSalonist at The Mane Event\nRoyal guard (briefly)",
+          kind: ["Pegasus"],
+          image: [
+            "https://vignette.wikia.nocookie.net/mlp/images/1/14/Zephyr_Breeze_ID_S6E11.png/revision/latest?cb=20160605103015",
+          ],
+        },
+      ];
 
-      const { result } = renderHook(() => useUnicornsApi());
+      const { result } = renderHook(() => useUnicornsApi(), {
+        wrapper: ({ children }) => (
+          <UiContextWrapper>{children}</UiContextWrapper>
+        ),
+      });
 
-      const currentUnicorns = await result.current.getUnicorns();
+      const loadUnicorns = async () => {
+        const unicorns = await result.current.getUnicorns();
 
-      expect(currentUnicorns).toStrictEqual(expectedUnicorns);
+        return unicorns;
+      };
+
+      const unicorns = await loadUnicorns();
+
+      expect(unicorns).toStrictEqual(expectedUnicorns);
     });
   });
 });
