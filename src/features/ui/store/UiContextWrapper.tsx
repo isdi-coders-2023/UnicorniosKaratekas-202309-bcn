46eba4c1feb48fbc9db6c9995683a74cdba412a1
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-03 20:25:54 +0100

    Add isLoading function

--- src/features/ui/store/UiContextWrapper.tsx
+++ src/features/ui/store/UiContextWrapper.tsx
@@ -1,10 +1,16 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import UiContext from "./UiContext";
 
 const UiContextWrapper = ({
   children,
 }: PropsWithChildren): React.ReactElement => {
-  return <UiContext.Provider value={{}}>{children}</UiContext.Provider>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <UiContext.Provider value={{ isLoading, setIsLoading }}>
+      {children}
+    </UiContext.Provider>
+  );
 };
 
 export default UiContextWrapper;
