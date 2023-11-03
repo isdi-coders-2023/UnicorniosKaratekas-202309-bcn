b728e74ab9a99499bb2c3ac157f6e7e57f712691
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-03 20:18:18 +0100

    Reorder import links

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
 import AppStyled from "./AppStyled";
 import Header from "../Header/Header";
 import UnicornListPage from "../../pages/UnicornsList/UnicornsListPage";
 import NavMenu from "../NavMenu/NavMenu";
+import UiContext from "../../features/ui/store/UiContext";
+import Loading from "../Loading/Loading";
 
 const App = (): React.ReactElement => {
+  const { isLoading } = useContext(UiContext);
   return (
     <>
       <Header />
       <AppStyled>
+        {isLoading && <Loading />}
         <Routes>
           <Route path="/" element={<Navigate to="/unicorns" />} />
           <Route path="/unicorns" element={<UnicornListPage />} />
